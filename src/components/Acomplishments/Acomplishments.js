26ec22ef277a136185c888dc36930e72516c3390
ayushsingh0607 <ayushraj0607@gmail.com>
2022-08-15 11:58:51 +0530

    done

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,12 +1,13 @@
 import React from 'react';
-// import Image from '../../../public/images/img3.png';
 
 
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { Box, Boxes, BoxNum, BoxText, BoxImage } from './AcomplishmentsStyles';
 
 const data = [
-  {image:'/images/img3.png'}
+  {number: '' , text:'Flipkart Certificate', image:'/images/img3.png',url:'https://drive.google.com/file/d/1RD9UQvri_bmGFV7wyKPex9Fx4a19mFit/view?usp=sharing', isImage:true},
+  {number: '' , text:'GFG Certificate', image:'/images/img5.png',url:'https://drive.google.com/file/d/1Sr-2gGGqJpbouwObMQDgCldyvPbc-Oig/view?usp=sharing', isImage:true},
+  
 
 ];
 
@@ -15,10 +16,10 @@ const Acomplishments = () => (
     <SectionTitle>Personal Achievements</SectionTitle>
     <Boxes>
      { data.map(( card,index ) => ( 
-       <Box key={index} onClick={() => {window.open ('https://drive.google.com/file/d/1RD9UQvri_bmGFV7wyKPex9Fx4a19mFit/view?usp=sharing', '_blank')} }>
+       <Box key={index}>
+        {card.isImage && <BoxImage src={card.image} onClick={() =>{window.open(card.url, '_blank')}} />}
           <BoxNum>{card.number}</BoxNum>
          <BoxText>{card.text}</BoxText> 
-         <BoxImage src={data[index].image} />
        </Box>
      ))}
     </Boxes>
--- src/components/Acomplishments/AcomplishmentsStyles.js
+++ src/components/Acomplishments/AcomplishmentsStyles.js
@@ -72,6 +72,7 @@ export const BoxText = styled.p`
   line-height: 24px;
   letter-spacing: 0.02em;
   color: rgba(255, 255, 255, 0.75);
+  margin-top:20px;
 
   @media ${props => props.theme.breakpoints.md}{
     font-size: 16px;
